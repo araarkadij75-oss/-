720e1f0f62603c88211df66038dedf2baef31056
araarkadij75-oss <araarkadij75@gmail.com>
2026-09-20 10:11:43 +0300

    MASTER AI 18.17.12: enable incremental Google sync

--- cloud-config.js
+++ cloud-config.js
@@ -1,7 +1,6 @@
 window.MASTER_AI_CLOUD_CONFIG={enabled:true,workspaceId:"master-ai-beta",backendMode:'spark-firestore-rules',firebaseConfig:{"apiKey":"AIzaSyAGsejTtazJUCf7mGKknC8WJUt9BWygNHA","appId":"1:625548199395:web:fa508f939bdafd252b0cd6","authDomain":"master-ai-beta-9440599.firebaseapp.com","messagingSenderId":"625548199395","projectId":"master-ai-beta-9440599","projectNumber":"625548199395","storageBucket":"master-ai-beta-9440599.firebasestorage.app","version":"2"}};
-window.MASTER_AI_BUILD='18.17.11-CANDIDATE';
+window.MASTER_AI_BUILD='18.17.12-CANDIDATE';
 window.MASTER_AI_BETA=true;
 window.MASTER_AI_BETA_ONLINE=true;
 /* Load quota-safe patch before embedded Firebase adapter. */
-document.write('<scr'+'ipt src="./quota-safe-v181711.js?v=181711">'+'<'+ '/scr'+'ipt>');
-document.write('<scr'+'ipt src="./bridge-delete-v181711.js?v=181711">'+'<'+ '/scr'+'ipt>');
+document.write('<scr'+'ipt src="./quota-safe-v181712.js?v=181712">'+'<'+ '/scr'+'ipt>');
